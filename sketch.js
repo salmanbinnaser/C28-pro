8a9da47bab00be3bdebaeadfdecb27dd91402d93
salmanbinnaser <82106790+salmanbinnaser@users.noreply.github.com>
2021-06-22 13:27:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    crumpledPaper = new Paper();
+    crumpledPaper = new Paper(100,465);
 
     bin = createSprite(964,520,20,20);
     bin.addImage(binImg);
@@ -23,6 +23,8 @@ function setup(){
     binPart1 = new Dustbin(902,505,10,120);
     binPart2 = new Dustbin(962,565,130,10);
     binPart3 = new Dustbin(1024,505,10,120);
+    
+    Slingshot=new Throw(stones.body,{x:100,y:465})
 }
 
 function draw(){
